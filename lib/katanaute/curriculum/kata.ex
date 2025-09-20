acfcbc8bd15593dfaf8ddaa3d546351d2c25c92d
Mathieu Cizo <maciz@outlook.fr>
2025-09-20 03:03:29 +0200

    Add association between Kata and Session schemas

--- lib/katanaute/curriculum/kata.ex
+++ lib/katanaute/curriculum/kata.ex
@@ -1,12 +1,15 @@
 defmodule Katanaute.Curriculum.Kata do
   use Ecto.Schema
   import Ecto.Changeset
+  alias Katanaute.Training.Session
 
   schema "katas" do
     field :name, :string
 
     field :level, Ecto.Enum,
       values: [yellow: 1, orange: 2, green: 3, blue: 4, brown: 5, shodan: 6]
+
+    has_many :sessions, Session
 
     timestamps(type: :utc_datetime)
   end
