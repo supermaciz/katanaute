4d7e19d4f2b5637f48017d0b036ce968428e9d75
Mathieu Cizo <maciz@outlook.fr>
2025-12-17 16:13:25 +0100

    style(phoenix): mix format

--- katanaute/lib/katanaute_web/endpoint.ex
+++ katanaute/lib/katanaute_web/endpoint.ex
@@ -26,9 +26,9 @@ defmodule KatanauteWeb.Endpoint do
     gzip: not code_reloading?,
     only: KatanauteWeb.static_paths()
 
-    if Code.ensure_loaded?(Tidewave) do
-      plug Tidewave
-    end
+  if Code.ensure_loaded?(Tidewave) do
+    plug Tidewave
+  end
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
